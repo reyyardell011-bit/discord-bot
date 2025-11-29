674783a42f712aaac03fe00eef88f5a98cd70bf0
reyyardell011-bit <reyyardell011@gmail.com>
2025-11-29 14:26:54 +0700

    Create keepalive.js

---
+++ keepalive.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive.");
+});
+
+app.listen(3000, () => {
+  console.log("Keepalive server running on port 3000");
+});
